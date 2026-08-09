7a361d5c4b383df71026169aac868251fe95e42d
ענב אורן <shakedororen@gmail.com>
2026-08-09 12:00:42 +0300

    3.3: adding id api get and 404 status code if failed

--- part3/index.js
+++ part3/index.js
@@ -33,6 +33,16 @@ app.get('/info', (request, response) => {
   <p>${new Date()}</p>`)
 })
 
+app.get('/api/person/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
