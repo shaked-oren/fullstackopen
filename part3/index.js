a3266d623b7573ab2b7c6e46e50b9d939512ab36
shaked-oren <shakedororen@gmail.com>
2026-09-09 08:51:30 +0300

    3.8 see morgan logs

--- part3/index.js
+++ part3/index.js
@@ -4,7 +4,11 @@ const morgan = require('morgan')
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
